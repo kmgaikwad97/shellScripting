8e3230ca70860e04742bc52dc24a320c3c937ac4
kshitijwohlig <kshitij.gaikwad@wohlig.com>
2024-01-14 23:29:23 +0530

    first shellScript

---
+++ first.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+echo Hello world
+echo -e "\033[0;31m fail message here"
+echo -e "\033[0;32m fail message here"
+echo -e "\033[0;33m fail message here"
+
+echo -e "\033[0;31m: Red"
+echo -e "\033[0;32m: Green"
+echo -e "\033[0;33m: Yellow"
+echo -e "\033[0;34m: Blue"
+echo -e "\033[0;35m: Magenta"
+echo -e "\033[0;36m: Cyan"
+echo -e "\033[0;37m: White"
+
+
+
+echo " h
+a
+a
+a
+a"
+
+echo " h \
+a \
+a \
+a \
+a"
+
+echo " npm i nodemon \
+express \
+cors \
+jsonwebtoken \
+bcrypt"
+
+# Strong Code
+echo ' npm i nodemon \
+express \
+cors \
+jsonwebtoken \
+bcrypt'
+
+# \n = new line  \v = vertical line  \t tab
+echo -e "this backslash tab \t this backslash tab \t this backslash tab \t"
+echo -e "this vertical line \v this vertical line \v this vertical line \v"
+echo -e "this new line \n this new line \n this new line \n"
+
---
+++ variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#user defined variable
+#system defined variable
+name="kshitij"
+age=30
+echo $name
+echo $age
+echo "my name is $name and age is $age" 
